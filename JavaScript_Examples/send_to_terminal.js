6d4a1c0ad9c5b0c14f3083c90e29c53469ec34a3
Christopher Barnes <cbarnes7404@yahoo.com>
2020-03-07 22:58:32 -0800

    [add] serparate examples folders and their respected terminal output file

---
+++ JavaScript_Examples/send_to_terminal.js
@@ -0,0 +1,2 @@
+console.log("Hello world!!")
+console.log("Hello world!! I am now a computer programmer")
---
+++ Python_Examples/send_to_terminal.py
@@ -0,0 +1,2 @@
+print("Hello world!!")
+print("Hello world!! I am now a computer programmer")
