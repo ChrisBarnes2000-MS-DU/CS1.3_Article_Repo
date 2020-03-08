4c4cf2aaa1577022e041d4e69bd2f5d608db1c4a
Christopher Barnes <cbarnes7404@yahoo.com>
2020-03-08 01:01:07 -0800

    [add] variables examples

---
+++ JavaScript_Examples/variables.js
@@ -0,0 +1,7 @@
+intNum = 5
+floatNum = 700.6
+sentence = 'hello world'
+
+console.log(intNum)
+console.log(floatNum)
+console.log(sentence)
---
+++ Python_Examples/variables.py
@@ -0,0 +1,7 @@
+int_num = 5
+float_num = 700.6
+sentence = 'hello world'
+
+print(int_num)
+print(float_num)
+print(sentence)
